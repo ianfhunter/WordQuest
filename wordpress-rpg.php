d4abc26ce2d54e4730671fa50bdfd70d1506fa08
Ian Hunter <hunterif@tcd.ie>
2014-04-29 15:17:19 +0100

    metabox idle animation. Now has user-specific characters

--- wordpress-rpg.php
+++ wordpress-rpg.php
@@ -2,25 +2,43 @@
 /*
 Plugin Name: Wordpress RPG
 Plugin URI: 
-Description: Incetivize your blogging with RPG Meta-elements
+Description: Incentivize your blogging with RPG Meta-elements
 Author: Ian Hunter
 Version: 0.0
 Author URI: www.ianhunter.ie
 */
 
 
-#The info display in the top right hand corner
+#Returns a user's active quest
+function get_quest(){
+    $file = WP_PLUGIN_DIR."/Wordpress-RPG/experience" . get_current_user_id() . ".rpg"; 
+    $json = file_get_contents ( $file );
+    if ($json == ""){
+        $quest = "None";
+    }else{
+        $jsonD = json_decode($json);
+        $quest = $jsonD->{"quest"};
+    }
+    return $quest;
+}
+
+
+#The info display in the top right hand corner.
 function admin_info_header() {
-    $file = WP_PLUGIN_DIR."/Wordpress-RPG/experience.rpg"; 
-    $experience = file_get_contents ( $file );
+    $file = WP_PLUGIN_DIR."/Wordpress-RPG/experience".get_current_user_id().".rpg"; 
+    $json = file_get_contents ( $file );
+    $jsonD = json_decode($json);
+    $experience = $jsonD->{"total_experience"};
     $stats = calc_level($experience);
-    echo '<div style="float:right">Level ' . $stats[0] . ' Templar - ' . $stats[1] . '/' . $stats[2] .' Exp - 2 Quests</div>';
+    $quest = $jsonD->{"quest"};
+
+    echo '<div style="float:right">⚔ Level ' . $stats[0] . ' Templar - ' . $stats[1] . '/' . $stats[2] .' Exp - Quest: '. $quest.'</div>';
 }
 
 #Calculates what level you are given your experience
 #Level 1 = 40
 #Level N+1 = 1.6 * N
-
+#Returns: Level, Remaining Exp, Next Level EXP
 function calc_level($experience){
     $level = 500;
     $modifier = 1.6;
@@ -33,31 +51,68 @@ function calc_level($experience){
     return array($count,$experience,$level);
 }
 
-#Gives menu item in settings
-function rpg_menu() {
-    add_options_page( 'RPG Settings', 'Wordpress RPG', 'manage_options', 'wordpress-rpg', 'rpg_options' );
-}
-
-#Forms in settings menu item
-function rpg_options() {
-    if ( !current_user_can( 'manage_options' ) )  {
-        wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-    }
-    echo '<div class="wrap">';
-    echo '<p>Here is where the form would go if I actually had options.</p>';
-    echo '</div>';
-}
-
-#Adds experience upon posting
+#Adds experience upon posting, based on character count
 function add_experience() {
-    $file = WP_PLUGIN_DIR."/Wordpress-RPG/experience.rpg"; 
-    $experience = file_get_contents ( $file );
+    $file = WP_PLUGIN_DIR."/Wordpress-RPG/experience" . get_current_user_id() . ".rpg"; 
+    $json = file_get_contents ( $file );
+    if ($json == ""){
+        $experience = 0;
+        $quest = "None";
+    }else{
+        $jsonD = json_decode($json);
+        $experience = $jsonD->{"total_experience"};
+        $quest = $jsonD->{"quest"};
+    }
+
+    #Should check if quest is complete & Optionally assign a new task
+    #Quests should be a 'write a post with X tag/category' - Other types?
+
     $exp = strlen($_POST['content']) + $experience;
-    file_put_contents ( $file ,$exp );
+    $json = json_encode(array( 
+                               "total_experience" => $exp, 
+                               "quest" => $quest
+                              ));
+    file_put_contents ( $file ,$json );
 }
+
+#Activates a quest
+function add_quest($quest_title){
+$file = WP_PLUGIN_DIR."/Wordpress-RPG/experience" . get_current_user_id() . ".rpg"; 
+    $json = file_get_contents ( $file );
+    if ($json == ""){
+        $experience = 0;
+    }else{
+        $jsonD = json_decode($json);
+        $experience = $jsonD->{"total_experience"};
+    }
+
+    $json = json_encode(array( 
+                               "total_experience" => $experience, 
+                               "quest" => $quest_title
+                              ));
+    file_put_contents ( $file ,$json );
+}
+
+function quest_metabox(){
+    add_meta_box("rpg-metabox", "Wordpress RPG", draw_metabox, 'post', 'side', 'high');
+}
+function draw_metabox(){
+    #Would be really nice to rotate through some phrases here.
+    #And also have avatars based on your character
+    echo " <img src='".plugins_url()."/Wordpress-RPG/hero.gif' /> " . "Killing some slimes... " ;
+    #Credit: http://leon-murayami.deviantart.com/art/Illusion-of-Gaia-Will-XP-402827050
+
+    #maybe echo active quest.
+    #echo get_quest()
+
+}
+
 
 add_action('admin_bar_menu', 'admin_info_header');
-add_action('admin_menu', 'rpg_menu' );
+#add_action('admin_menu', 'rpg_menu' );
 add_action('publish_post', 'add_experience' ); 
+
+add_action('add_meta_boxes', 'quest_metabox' ); 
+add_action('save_post', 'save_quest_draft' ); 
 
 ?>
